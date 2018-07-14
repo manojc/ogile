bdabad51cda0b65edbe83981a56cc7901412b531
Manoj Chalode <chalodem@gmail.com>
2018-07-14 17:45:52 +0100

    master - UI set up
    Added routing, ng material & set up home page

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,16 +1,36 @@
+//angular modules
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from "@angular/router";
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+//external modules
+
+//components
+import { AppComponent } from './app/app.component';
+import { HomeComponent } from './home/home.component';
+
+//services
+
+//pipes
+
+//routes (always last)
+import { Routes } from './app.routes';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    imports: [
+        BrowserModule,
+        NoopAnimationsModule,
+        MatToolbarModule,
+        //always last
+        RouterModule.forRoot(Routes)
+    ],
+    declarations: [
+        AppComponent,
+        HomeComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ ui/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Route } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
+
+export let Routes: Array<Route> = [
+    {
+        path: "",
+        component: HomeComponent
+    }
+];
